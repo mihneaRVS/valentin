7cfbdf51a9f717439925dc4c1319ed755d5451b6
sevr nehnea <159069200+mihneaRVS@users.noreply.github.com>
2024-02-05 19:34:21 +0000

    y

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,20 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
+      "nu",
+      "sigur?",
+      "foarte sigur?",
+      "mai gandestete",
+      "stai putin",
+      "sigur ca nu?",
+      "poate o sa iti pare rau",
+      "ba",
+      "esti 95% sigur?",
+      "sotto?",
+      "hama",
+      "haba",
       "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "pis pe tine",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
